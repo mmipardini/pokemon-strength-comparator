69505251b55f8ce3fd005ec81350fc9a66b2b95b
mmipardini <micaela.mqs@gmail.com>
2024-10-16 15:01:47 -0300

    fix: update CORS config to allow requests from frontend

--- back-end/PokemonAPI/Program.cs
+++ back-end/PokemonAPI/Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPokemonService, PokemonService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
